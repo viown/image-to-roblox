44eed16f0d0d1d918263837f2fa18893ef5bf2df
viown <48097677+viown@users.noreply.github.com>
2020-06-30 14:30:38 +0300

    Fixed sizing and blue light

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ def convert(image):
     image = Image.open(f"images/{image}")
     rows = image.size[0]
     col = image.size[1]
-    MAIN = model.Main((rows * 2) - 2, col * 1.5)
+    MAIN = model.Main((rows * 2) - 2, (col * 1.5) + 40.5)
 
     for y in range(1, col):
         for x in range(1, rows):
             getpixel = image.getpixel((x, y))
-            MAIN += model.Pixel(getpixel[1], getpixel[2], getpixel[3])
+            MAIN += model.Pixel(getpixel[0], getpixel[1], getpixel[2])
 
     return MAIN
 
